c5a8ebe82504b960a24c1cdf9eea1f05132fa1bf
Srinidhi Kaushik <shrinidhi.kaushik@gmail.com>
2018-12-18 00:45:22 +0530

    Formatter nitpicks.

--- main.go
+++ main.go
@@ -2,30 +2,30 @@
 package main
 
 import (
-    "os"
-    "flag"
-    "fmt"
+	"flag"
+	"fmt"
+	"os"
 
-    "github.com/clickyotomy/ext-ip/resolve"
-    "github.com/davecgh/go-spew/spew"
+	"github.com/clickyotomy/ext-ip/resolve"
+	"github.com/davecgh/go-spew/spew"
 )
 
 var spewConf = spew.ConfigState{Indent: "\t"}
 
 func usage() {
-    fmt.Fprintf(os.Stderr, "ext-ip: Fetch external IP address.\n")
+	fmt.Fprintf(os.Stderr, "ext-ip: Fetch external IP address.\n")
 }
 
 func main() {
-    flag.Usage = usage
-    flag.Parse()
+	flag.Usage = usage
+	flag.Parse()
 
-    addr, _, err := resolve.ExtIP()
+	addr, _, err := resolve.ExtIP()
 
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "%s\n", err)
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
 
-    fmt.Fprintf(os.Stdout, "%s\n", addr)
+	fmt.Fprintf(os.Stdout, "%s\n", addr)
 }
